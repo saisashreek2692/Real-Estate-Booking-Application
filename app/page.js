b14ed63602ae7c5ff7384011739ff74afc6be75b
Sai Sashreek D <saisashreekd@gmail.com>
2024-07-06 12:40:35 +0530

    Font Type Updated

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-rose-600 font-medium text-center text-3xl">Lets Create Real Estate Application</h1>
+      <h1 className="text-rose-600 font-normal text-center text-3xl">Lets Create Real Estate Application</h1>
     </>
   );
 }

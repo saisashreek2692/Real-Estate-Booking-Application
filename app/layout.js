33cd36694eb2e75de6816a2374dac8f397f00086
Sai Sashreek D <saisashreekd@gmail.com>
2024-07-10 14:25:21 +0530

    Updated Layout Page

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,21 @@
 import { Jost } from "next/font/google";
 import "./globals.css";
+import Provider from "./Provider";
 
 const jost = Jost({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Real Estate Booking Application",
-  description: "Sell, Rent, Book your House by creating an account and manage your Listings",
+  description:
+    "Sell, Rent, Book your House by creating an account and manage your Listings",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jost.className}>{children}</body>
+      <body className={jost.className}>
+        <Provider>{children}</Provider>
+      </body>
     </html>
   );
 }

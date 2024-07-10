869f6dfc6c34d0d567b986083feb8cedf5ea1d34
Sai Sashreek D <saisashreekd@gmail.com>
2024-07-10 14:25:38 +0530

    Updated Main Page with Provider

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-rose-600 font-normal text-center text-3xl">Lets Create Real Estate Application</h1>
     </>
   );
 }

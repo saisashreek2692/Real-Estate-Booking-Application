f06fce629397e3cbd6f1b9bee639cf977b367576
Sai Sashreek D <saisashreekd@gmail.com>
2024-07-10 14:25:09 +0530

    Added Custom Config from ShadCN

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#E90074",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {

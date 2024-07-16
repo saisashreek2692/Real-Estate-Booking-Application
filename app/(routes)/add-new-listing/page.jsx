11c0559b9186bac284a5403cd8f66a24cdc22e03
Sai Sashreek D <saisashreekd@gmail.com>
2024-07-16 17:14:10 +0530

    Added Routing in Add New Listing Page

--- app/(routes)/add-new-listing/page.jsx
+++ app/(routes)/add-new-listing/page.jsx
@@ -4,36 +4,41 @@ import { Button } from "@/components/ui/button";
 import { supabase } from "@/utils/supabase/client";
 import { useUser } from "@clerk/nextjs";
 import { Loader } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { toast } from "sonner";
 
 function AddNewListing() {
   const [selectedAddress, setSelectedAddress] = useState();
   const [coordinates, setCoordinates] = useState();
-  const [loader, setLoader] = useState(false)
- const {user} = useUser();
+  const [loader, setLoader] = useState(false);
+  const { user } = useUser();
+  const router = useRouter();
 
   const coordinateHandler = async () => {
-
+    setLoader(true);
     const { data, error } = await supabase
       .from("listing")
-      .insert([{ 
-        address: selectedAddress.label,
-        coordinates: coordinates,
-        createdBy: user?.primaryEmailAddress.emailAddress
-     }])
+      .insert([
+        {
+          address: selectedAddress.label,
+          coordinates: coordinates,
+          createdBy: user?.primaryEmailAddress.emailAddress,
+        },
+      ])
       .select();
 
-      if(data) {
-        setLoader(false);
-        console.log("New Data Added", data)
-        toast("New Record Added for Listing")
-      }
-      if (error) {
-        setLoader(false);
-        console.log("Failed To Add Record", error)
-        toast("Failed to Add Address")
-      }
+    if (data) {
+      setLoader(false);
+      console.log("New Data Added", data);
+      toast("New Record Added for Listing");
+      router.replace('/edit-listing/'+data[0].id)
+    }
+    if (error) {
+      setLoader(false);
+      console.log("Failed To Add Record", error);
+      toast("Failed to Add Address");
+    }
   };
 
   return (
